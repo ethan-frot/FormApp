d4caa57c8aa9431637e161ea3ac63e9b760a7b08
Ethan <github.xdgcd@passmail.net>
2024-10-16 13:43:25 +0200

    🥅Change type of password input

--- src/components/steps/PersonalInformation.tsx
+++ src/components/steps/PersonalInformation.tsx
@@ -1,28 +1,23 @@
 import Input from "../ui/Input.tsx";
 
 const PersonalInformation = () => {
-    return (
-        <div className="flex flex-col gap-8">
-            <Input
-                type='text'
-                placeholder='Jhon Doe'
-                name='name'
-                label='Name'
-            />
-            <Input
-                type='email'
-                placeholder='johndoe@gmail.com'
-                name='email'
-                label='Email'
-            />
-            <Input
-                type='text'
-                placeholder='********'
-                name='password'
-                label='Password'
-            />
-        </div>
-    );
+  return (
+    <div className="flex flex-col gap-8">
+      <Input type="text" placeholder="Jhon Doe" name="name" label="Name" />
+      <Input
+        type="email"
+        placeholder="johndoe@gmail.com"
+        name="email"
+        label="Email"
+      />
+      <Input
+        type="password"
+        placeholder="********"
+        name="password"
+        label="Password"
+      />
+    </div>
+  );
 };
 
 export default PersonalInformation;
